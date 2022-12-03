3d7041d9b1d0251301b67b38296a6e36c7cc03dd
drrelyea <drrelyea@gmail.com>
2022-12-02 21:51:45 -0800

    Day 3 in full - oops

--- day3.py
+++ day3.py
@@ -14,3 +14,30 @@ data = [
     for line in inpstring
 ]
 
+# the_sum = 0
+# for line in data:
+#     linelen = len(line)
+#     first_set = set(line[0:int(linelen/2)])
+#     second_set = set(line[int(linelen/2):])
+#     intersect = first_set.intersection(second_set)
+#     the_char = list(intersect)[0]
+#     if the_char in 'qwertyuiopasdfghjklzxcvbnmn':
+#         the_sum += ord(the_char) - 96
+#     else:
+#         the_sum += ord(the_char) - 38
+# print(the_sum)
+
+the_sum = 0
+for dd in range(int(len(data)/3)):
+    line1 = set(data[dd*3])
+    line2 = set(data[dd*3+1])
+    line3 = set(data[dd*3+2])
+    intersect = line1.intersection(line2)
+    intersect = intersect.intersection(line3)
+    print(intersect)
+    the_char = list(intersect)[0]
+    if the_char in 'qwertyuiopasdfghjklzxcvbnmn':
+        the_sum += ord(the_char) - 96
+    else:
+        the_sum += ord(the_char) - 38
+print(the_sum)
