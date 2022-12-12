afed0f8a331e8af3f4d4b85fdede411e55e1cccb
drrelyea <drrelyea@gmail.com>
2022-12-11 21:46:13 -0800

    LOL hand solution ftw

---
+++ day12.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+the_day = 12
+download_data_path = Path("/Users/relyea/Downloads/input.txt")
+local_data_path = "/Users/relyea/code/advent_of_code_2022/input"+str(the_day)+".txt"
+if download_data_path.exists():
+    download_data_path.rename(local_data_path)
+with open(local_data_path) as input_file:
+    inpstring = input_file.readlines()
+
+data = [
+    line.strip()
+    for line in inpstring
+]
+
+# data = open(local_data_path).read().strip().split("\n")
+
+LOL I solved this by eye
